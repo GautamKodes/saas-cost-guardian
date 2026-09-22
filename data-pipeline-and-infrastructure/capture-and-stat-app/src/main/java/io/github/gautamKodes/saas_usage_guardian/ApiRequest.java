cbf4854642ff9747881fc6842022453cade6b173
GautamKodes <gautam6605k@proton.me>
2026-09-22 17:30:32 +0530

    Configured docker for zookeeper, kafka and redis

---
+++ data-pipeline-and-infrastructure/capture-and-stat-app/src/main/java/io/github/gautamKodes/saas_usage_guardian/ApiRequest.java
@@ -0,0 +1,4 @@
+package io.github.gautamKodes.saas_usage_guardian;
+
+public class ApiRequest {
+}
---
+++ data-pipeline-and-infrastructure/capture-and-stat-app/src/main/java/io/github/gautamKodes/saas_usage_guardian/CaptureAndStatAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.gautamKodes.saas_usage_guardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptureAndStatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptureAndStatAppApplication.class, args);
+	}
+
+}
